3627eb2b713d4cc0901816487b81c80444427ecf
Vishnu Rajeevan <github@vishnu.email>
2026-06-11 13:24:41 -0400

    add flocon cask

---
+++ Casks/flocon.rb
@@ -0,0 +1,11 @@
+cask "flocon" do
+  version "1.8.2"
+  sha256 "6ca90caa1f6a712cd1faef69f6cbc1523f9aa9b5083ea100bbf17806639b7279"
+
+  url "https://github.com/openflocon/Flocon/releases/download/#{version}/macos-arm64.dmg"
+  name "flocon"
+  desc "desktop app for flocon inspector"
+  homepage "https://openflocon.github.io/Flocon/"
+
+  app "flocon.app"
+end
