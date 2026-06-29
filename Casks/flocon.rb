1b2cf62fe12d8c9230b3cf9e803c04ee6d9824df
Vishnu Rajeevan <github@vishnu.email>
2026-06-29 10:50:22 -0400

    Update Flocon version to 1.8.3

--- Casks/flocon.rb
+++ Casks/flocon.rb
@@ -1,5 +1,5 @@
 cask "flocon" do
-  version "1.8.2"
+  version "1.8.3"
   sha256 "6ca90caa1f6a712cd1faef69f6cbc1523f9aa9b5083ea100bbf17806639b7279"
 
   url "https://github.com/openflocon/Flocon/releases/download/#{version}/macos-arm64.dmg"
